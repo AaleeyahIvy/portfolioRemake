28e27e8232077b914646d97d62e9afe93353bff6
Aaleeyah Ivy-Kilgore <53322518+AaleeyahIvy@users.noreply.github.com>
2024-05-29 15:40:42 -0400

    finishing up project component and added images

--- src/components/base-components/MyProjectsComponent.vue
+++ src/components/base-components/MyProjectsComponent.vue
@@ -4,6 +4,7 @@
         <div v-for="project in projects" :key="project.title" class="project">
             <h2>{{ project.title }}</h2>
             <p>{{ project.description }}</p>
+            <img :src="project.image" alt="project image" class="project-image">
         </div>
         <div class="skills">
             <h1>My Skills:</h1>
@@ -23,6 +24,9 @@
 </template>
 <script>
 import anime from 'animejs/lib/anime.es.js';
+import portfolio from '@/assets/images/portfolio.png'
+import yinyang from '@/assets/images/yinyang.png'
+import github from '@/assets/images/github.png'
 
 export default {
     name: 'MyProjectsComponent',
@@ -31,16 +35,19 @@ export default {
             aboutpage: 'This is the Projects page',
             projects: [
                 {
-                    title: 'Legacy Website',
-                    description: 'This is the old portfolio, it was made with HTML, CSS, and pure JavaScript. It is hosted using NGINX and Docker containers'
+                    title: 'Legacy Portfolio',
+                    description: 'This is the old portfolio, it was made with HTML, CSS, and pure JavaScript. It is hosted using NGINX and Docker containers',
+                    image: portfolio
                 },
                 {
                     title: 'Yin Yang App',
-                    description: 'A web app similar to messenger apps for planning with friends that have differing schedules. '
+                    description: 'A web app similar to messenger apps for planning with friends that have differing schedules. ',
+                    image: yinyang
                 },
                 {
                     title: 'Github Repos',
-                    description: 'Curated list of my school projects and personal projects.'
+                    description: 'Curated list of my school and personal projects.',
+                    image: github
                 }
             ],
 
@@ -83,7 +90,7 @@ export default {
 }
 #skillHTML{
   width: auto;
-  background-color: #f16529;
+  background-color: #fde379;
   border-radius: 25px;
   margin: 15px;
   padding: 10px;
@@ -111,7 +118,7 @@ export default {
 }
 #skillSQL{
   width: auto;
-  background-color: #f29111;
+  background-color: #80f1c8;
   border-radius: 25px;
   margin: 15px;
   padding: 10px;
@@ -125,42 +132,42 @@ export default {
 }
 #skillVUE{
   width: auto;
-  background-color: #f16529;
+  background-color: #e48bff;
   border-radius: 25px;
   margin: 15px;
   padding: 10px;
 }
 #skillFIG{
   width: auto;
-  background-color: #f16529;
+  background-color: #dc8058;
   border-radius: 25px;
   margin: 15px;
   padding: 10px;
 }
 #skillCANVA{
   width: auto;
-  background-color: #f16529;
+  background-color: #3dcd57;
   border-radius: 25px;
   margin: 15px;
   padding: 10px;
 }
 #skillADOBE{
   width: auto;
-  background-color: #f16529;
+  background-color: #7777ff;
   border-radius: 25px;
   margin: 15px;
   padding: 10px;
 }
 #skillHTML{
   width: auto;
-  background-color: #f16529;
+  background-color: #c96379;
   border-radius: 25px;
   margin: 15px;
   padding: 10px;
 }
 #skillPRO{
   width: auto;
-  background-color: #f16529;
+  background-color: #8cff57;
   border-radius: 25px;
   margin: 15px;
   padding: 10px;
@@ -180,6 +187,12 @@ export default {
   margin: 20px;
   border: #171616 2px solid;
   border-radius: 10px;
+}
+.project-image{
+  width: 75%;
+  height: 50%;
+  border-radius: 25px;
+  padding: 15px;
 }
 
 </style>
